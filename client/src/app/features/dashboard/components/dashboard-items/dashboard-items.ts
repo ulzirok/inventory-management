7601387b1e2f9de572e88fbeef252729f1effb87
ulzirok <ulzirok@gmail.com>
2026-03-09 20:27:07 +0500

    Add loader to dashboard-list

--- client/src/app/features/dashboard/components/dashboard-items/dashboard-items.ts
+++ client/src/app/features/dashboard/components/dashboard-items/dashboard-items.ts
@@ -1,44 +1,46 @@
-import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { ItemsList } from '../../../inventory/components/items-list/items-list';
 import { InventoryService } from '../../../inventory/services/inventory.service';
 import { Item } from '../../../inventory/models/item.interface';
 import { ActivatedRoute } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Inventory } from '../../../inventory/models/inventory.interface';
+import { finalize, forkJoin } from 'rxjs';
+import { Loader } from '../../../../shared/components/loader/loader';
 
 @Component({
   selector: 'app-dashboard-items',
-  imports: [ItemsList],
+  imports: [ItemsList, Loader],
   templateUrl: './dashboard-items.html',
   styleUrl: './dashboard-items.scss',
 })
-export class DashboardItems {
+export class DashboardItems implements OnInit{
   private inventoryService = inject(InventoryService);
   private route = inject(ActivatedRoute)
   private destroyRef = inject(DestroyRef);
   
   inventory = signal<Inventory | null>(null);
   items = signal<Item[]>([]);
+  isLoading = signal(false);
   
   ngOnInit(): void {
+    this.isLoading.set(true); 
     const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.inventoryService.getById(id).pipe(
-        takeUntilDestroyed(this.destroyRef)
-      ).subscribe(
-        (data) => {
-          this.inventory.set(data);
-        }
-      );
-    }
+    if (!id) return
     
-    const inventoryId = this.route.snapshot.paramMap.get('id');
-    if (inventoryId) {
-      this.inventoryService.getPublicItems(inventoryId).pipe(
-        takeUntilDestroyed(this.destroyRef)
-      ).subscribe(
-        data => this.items.set(data)
-      );
-    }
+    forkJoin({
+      inventory: this.inventoryService.getById(id),
+      items: this.inventoryService.getPublicItems(id)
+    }).pipe(
+      takeUntilDestroyed(this.destroyRef),
+      finalize(()=> this.isLoading.set(false))
+    ).subscribe({
+      next: (data) => {
+        this.inventory.set(data.inventory)
+        this.items.set(data.items)
+      },
+      error: (err) => {}
+    })
   }
+  
 }
