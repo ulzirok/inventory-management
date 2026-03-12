da6c7822fc96754af2592affcf68719dd4e4ae84
ulzirok <ulzirok@gmail.com>
2026-03-12 15:52:51 +0500

    Refactor the inventory controller

---
+++ server/utils/createError.js
@@ -0,0 +1,5 @@
+module.exports = function createError(message, status) {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+};
--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -1,6 +1,12 @@
-module.exports = (res, err) => {
-    res.status(500).json({
+module.exports = (res, error) => {
+    const status = error.status || 500
+    res.status(status).json({
         success: false,
-        message: err.message ? err.message : err,
-    });
+        message: error.message || "Internal server error"
+    })
+    
+    // res.status(500).json({
+    //     success: false,
+    //     message: err.message ? err.message : err,
+    // });
 };
