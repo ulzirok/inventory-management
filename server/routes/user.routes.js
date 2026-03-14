ca737ceac0d5aa7e1eae8db4d1748156085ea60f
ulzirok <ulzirok@gmail.com>
2026-03-14 19:14:21 +0500

    User-details access refactor

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const { isAdmin } = require("../middleware/role");
+const { isAuthenticated } = require("../middleware/role");
 const controller = require("../controllers/user.controller");
 
+
+router.get("/:id", passport.authenticate("jwt", { session: false }), isAuthenticated, controller.getUserById);
 router.use(passport.authenticate("jwt", { session: false }), isAdmin);
 
 router.get("/", controller.getAll);
-router.get("/:id", controller.getUserById);
 router.patch("/status", controller.changeStatus);
 router.patch("/role", controller.changeRole);
 router.post("/delete", controller.delete);
